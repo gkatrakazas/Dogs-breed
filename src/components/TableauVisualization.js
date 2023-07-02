3a66f536124712895a81e0b43ef0e0030c0a3518
Grigoris Katrakazas <grigoriskatrakazas@gmail.com>
2023-07-02 17:00:39 +0300

    test with tablue

--- src/components/TableauVisualization.js
+++ src/components/TableauVisualization.js
@@ -1,28 +1,41 @@
 import React, { useRef, useEffect } from 'react';
 import '../css/Tableau.css';
 
-const {tableau} = window;
+const { tableau } = window;
+
 function TableauVisualization() {
+  const viz1Ref = useRef(null);
+  const viz2Ref = useRef(null);
+  const viz1Url = 'https://public.tableau.com/views/Dog-breeds-visualization/Dashboard1';
+  const viz2Url = 'https://public.tableau.com/views/Dog-breeds-visualization2/Dashboard2';
 
-    const ref = useRef(null);
-    const url ='https://public.tableau.com/views/Dog-breeds-visualization/Dashboard1';
+  const initViz = (ref, url) => {
+    const viz = new tableau.Viz(ref.current, url, {
+      width: '100%',
+      height: '150vh',
+    });
 
-    const initViz = () => {
-        const existingViz = tableau.VizManager.getVizs()[0]; // Get the existing viz
-        if (existingViz) {
-          existingViz.dispose(); // Dispose of the existing viz
-        }
-      
-        new tableau.Viz(ref.current, url, {
-          width: '100%',
-          height: '90vh',
-        });
-      };
-      
+    return () => {
+      viz.dispose();
+    };
+  };
 
-    useEffect(initViz,[]);
+  useEffect(() => {
+    const cleanup1 = initViz(viz1Ref, viz1Url);
+    const cleanup2 = initViz(viz2Ref, viz2Url);
 
-  return <div ref={ref}/>;
+    return () => {
+      cleanup1();
+      cleanup2();
+    };
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column',flexWrap:'nowrap' }}>
+      <div ref={viz1Ref} style={{ textAlign: 'center' }} />
+      <div ref={viz2Ref} style={{ textAlign: 'center' }} />
+    </div>
+  );
 }
 
 export default TableauVisualization;
--- src/pages/visualization.js
+++ src/pages/visualization.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import "../css/Visualization.css";
 import TableauVisualization from '../components/TableauVisualization';
-import TableauVisualization2 from '../components/TableauVisualization2';
 
 function visualization() {
   return (
@@ -21,14 +20,8 @@ function visualization() {
         <p>Step into our visualization page, a gateway to a visual journey through the complex dynamics of breed characteristics, health problems, and life expectancy. Here, we present an immersive experience that combines art and science to unravel the hidden insights within the data. Through interactive charts, graphs, and visual representations, we aim to provide a comprehensive understanding of the intricate interplay between breed traits, health issues, and the longevity of our canine companions. Explore our dynamic visualizations that allow you to filter, analyze, and explore the data from multiple perspectives. Witness how different breed characteristics contribute to health problems and how these factors impact the life expectancy of various breeds. Delve into the correlations and patterns that emerge, enabling you to gain valuable insights into breed selection, health management, and overall well-being. Whether you're a dog enthusiast seeking a deeper understanding of your furry friend or a professional in the field, our visualizations offer a powerful tool for data-driven decision-making and a gateway to a more informed and compassionate approach to dog care. Join us on this visual voyage and unlock a new level of understanding in the world of dogs.</p>      
       </div>
       <div className='dashboard1'>
-      <TableauVisualization />
-
+        <TableauVisualization />
       </div>
-      {/* <div className='dashboard2'>
-      <TableauVisualization2 />
-
-      </div> */}
-
     </div>
   );
 }
